b8a8138c6fbbd81cae64b2332661c6383da88fa1
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-26 18:24:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ end
   def self.find_or_create_by(dog_object)
     dog_name = dog_object[:name]
     if self.find_by_name(dog_name)
-      self.find_by_name(dog_name)
+       returnself.find_by_name(dog_name)
     else
       new_dog = self.create(dog_object)
       new_dog.save
